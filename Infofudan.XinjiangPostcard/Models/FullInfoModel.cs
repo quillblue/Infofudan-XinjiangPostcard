ff7d8a940fbc947df6a7f9511c2021b4ebc5c0c9
quillblue <sheng.qian@outlook.com>
2014-12-25 14:58:17 +0800

    Daily Commit

---
+++ Infofudan.XinjiangPostcard/Models/FullInfoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Infofudan.XinjiangPostcard.Models
+{
+    public class FullInfoModel
+    {
+        public Postcard MainInfo;
+        public Place SenderPlace;
+        public Place PhotoPlace;
+
+
+    }
+}
